fb5f491a0ee8947d30c92d19bae52fd5d446780b
Yajayra <yajayra.arauco@gmail.com>
2020-12-12 23:14:08 -0500

    article one

--- 2.Programacion-basica-platzi/20.uso-carga-img-en-canvas/VillaPlatzi/platzi.js
+++ 2.Programacion-basica-platzi/20.uso-carga-img-en-canvas/VillaPlatzi/platzi.js
@@ -1,14 +1,46 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
-var mapa = "tile.png";
+/* "tile.png"; --->llamamos la imagen*/
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+var fondo = {
+  url: "tile.png",
+  cargaOK: false
+};
 
-function dibujar()
+var vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
+
+fondo.imagen = new Image(); /*--> cargamos la imagen*/
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo); /*el escuchador cuando detecta que la imagen ha sido cargada inicia la función dibujar*/
+
+vaca.imagen = new Image();
+vaca.imagen.scr = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+function cargarFondo()
 {
-  papel.drawImage(imagen, 0, 0);
+  fondo.cargaOK = true;
+  dibujar();
+}
+
+function cargarVacas()
+{
+  vaca.cargaOK = true;
+  dibujar();
+}
+
+function dibujar() {
+  if (fondo.cargaOK == true)
+  {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  if (vaca.cargaOK == true)
+  {
+    papel.drawImage(vaca.imagen, 100, 0);
+  }
 }
 
 function aleatorio(min,maxi)
